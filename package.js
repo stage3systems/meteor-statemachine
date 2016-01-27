f51fe13cd73ec79e25da1c4b4d7e13725b092d0e
Ryan McCuaig <ryan@ryanmccuaig.net>
2016-01-26 20:59:43 -0800

    Fixes server
    See https://github.com/nate-strauser/meteor-statemachine/pull/3/

--- package.js
+++ package.js
@@ -1,11 +1,15 @@
 Package.describe({
-  name: "natestrauser:statemachine",
+  name: "stage3systems:statemachine",
   summary: "Javascript State Machine packaged for meteor",
   version: "2.3.5",
-  git: "https://github.com/nate-strauser/meteor-statemachine.git"
+  git: "https://github.com/stage3systems/meteor-statemachine.git"
 });
 
 Package.on_use(function(api) {
   api.versionsFrom("METEOR@0.9.0");
-  api.add_files(['lib/javascript-state-machine/state-machine.js'], ['client', 'server']);
+  api.add_files([
+    'lib/prefix.js',
+    'lib/javascript-state-machine/state-machine.js',
+    'lib/suffix.js'
+  ], ['client', 'server']);
 });
